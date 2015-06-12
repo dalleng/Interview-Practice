0c6d576401baac667d522d1000315d3d3311cbf1
Diego Allen <diegoallen@gmail.com>
2015-06-12 15:40:37 -0400

    Add 'Remove Linked List Elements' and 'Reverse Bits' problems from leetcode

---
+++ leetcode/Remove Linked List Elements/main.py
@@ -0,0 +1,85 @@
+import unittest
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __str__(self):
+        s = '{}'.format(self.val)
+        n = self.next
+        while n:
+            s += ' -> {}'.format(n.val)
+            n = n.next
+        return s
+
+    __repr__ = __str__
+
+    def __eq__(self, other):
+        return str(self) == str(other)
+
+
+class Solution:
+    # @param {ListNode} head
+    # @param {integer} val
+    # @return {ListNode}
+    def removeElements(self, head, val):
+        if head is None:
+            return None
+
+        while head and head.val == val:
+            head = head.next
+
+        current = head
+        previous = head
+
+        while current:
+            if current == previous:
+                current = current.next
+            elif current.val == val:
+                current = current.next
+                previous.next = current
+            else:
+                previous = current
+                current = current.next
+
+        return head
+
+
+def buildLinkedList(iterable):
+    root = ListNode(iterable[0])
+    tail = root
+    for val in iterable[1:]:
+        node = ListNode(val)
+        tail.next = node
+        tail = node
+    return root
+
+
+class RemoveElementsTest(unittest.TestCase):
+    s = Solution()
+
+    def test_val_front(self):
+        l1 = buildLinkedList('66234')
+        l2 = buildLinkedList('234')
+        self.assertEquals(self.s.removeElements(l1, '6'), l2)
+
+    def test_val_middle(self):
+        l1 = buildLinkedList('26634')
+        l2 = buildLinkedList('234')
+        self.assertEquals(self.s.removeElements(l1, '6'), l2)
+
+    def test_val_end(self):
+        l1 = buildLinkedList('23466')
+        l2 = buildLinkedList('234')
+        self.assertEquals(self.s.removeElements(l1, '6'), l2)
+
+    def test_only_val(self):
+        l1 = buildLinkedList('66')
+        r = self.s.removeElements(l1, '6')
+        self.assertEquals(r, None)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ leetcode/Reverse Bits/main.py
@@ -0,0 +1,30 @@
+import unittest
+
+
+class Solution:
+    # @param n, an integer
+    # @return an integer
+
+    def reverseBits(self, n):
+        mask = 1
+        reverse = 0
+
+        for i in range(32):
+            offset = 31 - 2 * i
+            if offset > 0:
+                reverse |= (n & (mask << i)) << offset
+            else:
+                reverse |= (n & (mask << i)) >> abs(offset)
+
+        return reverse
+
+
+class TestReverseBits(unittest.TestCase):
+    s = Solution()
+
+    def test_example(self):
+        self.assertEquals(self.s.reverseBits(43261596), 964176192)
+
+
+if __name__ == '__main__':
+    unittest.main()
