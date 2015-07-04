565cc6791cc002062f62de4c498bed1550929fd2
Diego Allen <diegoallen@gmail.com>
2015-07-03 22:32:19 -0400

    Add Excel Sheet Column Number from leetcode

---
+++ leetcode/Excel Sheet Column Number/main.py
@@ -0,0 +1,29 @@
+import unittest
+from string import uppercase
+
+
+class Solution:
+    # @param {string} s
+    # @return {integer}
+    def titleToNumber(self, s):
+        if not s:
+            return 0
+        else:
+            return sum((uppercase.index(letter) + 1) * 26 ** i
+                       for i, letter in enumerate(reversed(s)))
+
+
+class TitleToNumberTest(unittest.TestCase):
+    def test_single_letter(self):
+        s = Solution()
+        for i in uppercase:
+            self.assertEquals(s.titleToNumber(i), uppercase.index(i) + 1)
+
+    def test_two_letters(self):
+        s = Solution()
+        self.assertEquals(s.titleToNumber('AA'), 27)
+        self.assertEquals(s.titleToNumber('AB'), 28)
+
+
+if __name__ == '__main__':
+    unittest.main()
