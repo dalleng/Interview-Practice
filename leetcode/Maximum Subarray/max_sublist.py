dd1c42dac1fa3ee8301569d9bbc68aee997c681c
Diego Allen <diegoallen@gmail.com>
2015-07-11 16:58:41 -0400

    Put function inside Solution class like all leetcode solutions

--- leetcode/Maximum Subarray/max_sublist.py
+++ leetcode/Maximum Subarray/max_sublist.py
@@ -1,13 +1,17 @@
-def max_sublist(nums):
-    mem = []
-    max_sum = None
-    for i in range(len(nums)):
-        current_sum = max(0, mem[i-1] if i > 0 else 0) + nums[i]
-        if max_sum is None or current_sum > max_sum:
-            max_sum = current_sum
-        mem.append(current_sum)
-    return max_sum
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+    def maxSubArray(self, nums):
+        mem = []
+        max_sum = None
+        for i in range(len(nums)):
+            current_sum = max(0, mem[i-1] if i > 0 else 0) + nums[i]
+            if max_sum is None or current_sum > max_sum:
+                max_sum = current_sum
+            mem.append(current_sum)
+        return max_sum
 
-assert max_sublist([-1, 0, -2]) == 0
-assert max_sublist([-1, 5, 100, -1000]) == 105
-assert max_sublist([4, -1, 2, 1]) == 6
+
+assert Solution().maxSubArray([-1, 0, -2]) == 0
+assert Solution().maxSubArray([-1, 5, 100, -1000]) == 105
+assert Solution().maxSubArray([4, -1, 2, 1]) == 6
