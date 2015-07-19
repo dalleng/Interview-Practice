10269e8532c4c075e2bb9c80edf1a695bd3114f0
Diego Allen <diegoallen@gmail.com>
2015-07-19 10:05:10 -0400

    Add problem 'Check if all leaves are at the same level'

---
+++ leaves-same-level/main.py
@@ -0,0 +1,58 @@
+import unittest
+
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+
+    @classmethod
+    def node_with_children(cls, val, left, right):
+        node = cls(val)
+        node.left = left
+        node.right = right
+        return node
+
+
+def check_leaves_levels(root):
+    """
+    Check if all leaves are at the same level
+    """
+    levels = []
+
+    def depth_first_traversal(root, level):
+        if not root:
+            return
+
+        if root.right is None and root.left is None:
+            levels.append(level)
+        else:
+            depth_first_traversal(root.left, level + 1)
+            depth_first_traversal(root.right, level + 1)
+
+    depth_first_traversal(root, 0)
+    return len(set(levels)) == 1
+
+
+class CheckLeavesLevels(unittest.TestCase):
+    def test_single_node(self):
+        root = TreeNode('foo')
+        self.assertTrue(check_leaves_levels(root))
+
+    def test_different_levels(self):
+        """
+            root
+            / \
+           1   2
+          /
+         3
+        """
+        node1 = TreeNode.node_with_children(1, TreeNode(3), None)
+        node2 = TreeNode(2)
+        root = TreeNode.node_with_children(0, node1, node2)
+        self.assertFalse(check_leaves_levels(root))
+
+
+if __name__ == '__main__':
+    unittest.main()
