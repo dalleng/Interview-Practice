2e2920f13b6b6d03237791cf4cabe569708eb96e
Diego Allen <diegoallen@gmail.com>
2017-01-20 15:38:00 -0300

    Add Sparse Arrays problem from Hacker Rank

---
+++ HackerRank/Data Structures/Arrays/Sparse Arrays/sparse_arrays.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from collections import Counter
+
+
+def main():
+    ctr = Counter()
+    N = int(input().strip())
+
+    for _ in range(N):
+        s = input().strip()
+        ctr[s] += 1
+
+    Q = int(input().strip())
+    for _ in range(Q):
+        q = input().strip()
+        print(ctr[q])
+
+
+if __name__ == '__main__':
+    main()
