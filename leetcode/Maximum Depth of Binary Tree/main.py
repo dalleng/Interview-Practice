1ac478ecb4522681069ffb5e0bc4eeda21e796b1
Diego Allen <diegoallen@gmail.com>
2021-07-18 21:26:06 -0400

    Add Maximum Depth of Binary Tree exercise from leetcode

---
+++ leetcode/Maximum Depth of Binary Tree/main.py
@@ -0,0 +1,10 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        return max(self.maxDepth(root.left) + 1, self.maxDepth(root.right) + 1) if root else 0
