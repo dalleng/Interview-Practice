6c30ec0770a93a2f7e35848e757fc740c7187cb7
Diego Allen <diegoallen@gmail.com>
2022-10-13 10:21:02 -0300

    Add additional solution for 3Sum problem

--- leetcode/3Sum/main.py
+++ leetcode/3Sum/main.py
@@ -32,3 +32,27 @@ class Solution:
 
             self.twoSum(nums, -n, i + 1, res)
         return res
+
+
+# Solution to the 3Sum problem that does not require sorting
+class Solution2:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        solutions = []
+        solutions_seen = set()
+        for i, n in enumerate(nums[:-2]):
+            opposite = n * -1
+            for solution in self.twoSum(nums, opposite, i + 1):
+                if solution:
+                    solution.append(n)
+                    solution.sort()
+                    if str(solution) not in solutions_seen:
+                        solutions.append(solution)
+                        solutions_seen.add(str(solution))
+        return solutions
+
+    def twoSum(self, nums: List[int], target: int, lo: int) -> List[List[int]]:
+        seen = set()
+        for n in nums[lo:]:
+            if target - n in seen:
+                yield [n, target - n]
+            seen.add(n)
